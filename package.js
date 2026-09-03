47e039a5ae80871e1af03bf78f2854e1475c0d4c
visa <309810744+visadaking@users.noreply.github.com>
2026-09-03 19:22:53 +0300

    Create package.js
    Updated package.js to version 1.7.6

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "nuclear-player-plugin",
+  "version": "1.7.6",
+  "description": "Nuclear Mini Player, synced and lyrics and click-to-seek, draggable resizable panel, full-screen view, text resizing, copying lyrics, and manual refreshing",
+  "author": "visa",
+  "main": "index.js",
+  "nuclear": {
+    "displayName": "Nuclear Lyrics",
+    "categories": [
+      "lyrics",
+      "ui"
+    ]
+  }
+}
