0d4a83db657b856c7d0610a2662083a04358486d
visadaking <yahalom.mauni@protonmail.com>
2026-09-05 02:48:36 +0300

    version 1.7.7 update

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 {
-  "name": "nuclear-player-plugin",
+  "name": "nuclear-mini-player",
   "version": "1.7.6",
   "description": "Nuclear Mini Player, synced and lyrics and click-to-seek, draggable resizable panel, full-screen view, text resizing, copying lyrics, and manual refreshing",
   "author": "visa",
   "main": "index.js",
   "nuclear": {
-    "displayName": "Nuclear Lyrics",
+    "displayName": "miniplayer",
     "categories": [
       "lyrics",
-      "ui"
+      "other"
     ]
   }
 }
